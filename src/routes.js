ac72e9dd9a38391c7b0d5f284e763ea61a7ddffe
VictorlReis <victorsireis@gmail.com>
2020-02-18 22:41:06 -0300

    User model and index.js

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({message: 'Teste'});
-})
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Victor Reis',
+    email: 'victorsireis@gmail.com',
+    password_hash: '1234',
+  });
+  return res.json(user);
+});
 
 export default routes;
